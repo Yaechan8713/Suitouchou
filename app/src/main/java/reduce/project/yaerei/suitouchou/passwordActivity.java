99580e4f025be033423e9733c8e39023cef770b4
yaerei <yae.tetsuo@gmail.com>
2018-02-25 00:23:10 +0900

    passwordActivityの作成

---
+++ app/src/main/java/reduce/project/yaerei/suitouchou/passwordActivity.java
@@ -0,0 +1,143 @@
+package reduce.project.yaerei.suitouchou;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by yaerei on 2018/02/24.
+ */
+
+public class passwordActivity extends AppCompatActivity {
+    String maru;
+
+    int password,nyuuryoku;
+
+    TextView passwordtextView;
+
+    SharedPreferences sharedPreferences;
+
+    SharedPreferences.Editor editor;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.password_main);
+
+        int passwordfirst = 0;
+
+        sharedPreferences = getSharedPreferences("passwordfirst",Context.MODE_PRIVATE);
+        passwordfirst = sharedPreferences.getInt("passwordfirst",0);
+
+        if(passwordfirst == 0){
+            intent = new Intent(this,passwordinputActivity.class);
+            startActivity(intent);
+            finish();
+         }
+
+        password = nyuuryoku = 0;
+
+        sharedPreferences = getSharedPreferences("password", Context.MODE_PRIVATE);
+        password = sharedPreferences.getInt("password",0);
+
+        passwordtextView = (TextView)findViewById(R.id.passwordtextView);
+        maru = "";
+
+    }
+
+    public void passwordrun(int passwordnyuuryoku){
+        maru = maru + "●";
+
+        passwordtextView.setText(maru);
+
+        nyuuryoku = nyuuryoku * 10 + passwordnyuuryoku;
+    }
+
+    public void ok(View v){
+        if(password == nyuuryoku){
+            new AlertDialog.Builder(passwordActivity.this)
+                    .setTitle("ロック解除")
+                    .setMessage("ロックが解除されました。")
+                    .setPositiveButton(
+                            R.string.ok,
+
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    shakkinnActivityintent();
+                                }
+                            }
+                    ).show();
+        }else{
+            new AlertDialog.Builder(passwordActivity.this)
+                    .setTitle(R.string.error)
+                    .setMessage("パスワードが違います。")
+                    .setPositiveButton(
+                            R.string.ryoukai,
+
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    nyuuryoku = 0;
+                                }
+                            }
+                    ).show();
+        }
+    }
+
+    public void shakkinnActivityintent(){
+        intent = new Intent(this,shakkinnActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void num1(View v){
+        passwordrun(1);
+    }
+
+    public void num2(View v){
+        passwordrun(2);
+    }
+
+    public void num3(View v){
+        passwordrun(3);
+    }
+
+    public void num4(View v){
+        passwordrun(4);
+    }
+
+    public void num5(View v){
+        passwordrun(5);
+    }
+
+    public void num6(View v){
+        passwordrun(6);
+    }
+
+    public void num7(View v){
+        passwordrun(7);
+    }
+
+    public void num8(View v){
+        passwordrun(8);
+    }
+
+    public void num9(View v){
+        passwordrun(9);
+    }
+
+    public void num0(View v){
+        passwordrun(0);
+    }
+
+
+}
---
+++ app/src/main/java/reduce/project/yaerei/suitouchou/passwordinputActivity.java
@@ -0,0 +1,112 @@
+package reduce.project.yaerei.suitouchou;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by yaerei on 2018/02/24.
+ */
+
+public class passwordinputActivity extends AppCompatActivity {
+
+    TextView passwordinputtextView;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    int password;
+
+    String maru;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.passwordinput_main);
+
+        passwordinputtextView = (TextView)findViewById(R.id.passwordinputtextView);
+
+        maru = "";
+
+        password = 0;
+    }
+
+    public void passwordinput(int passwordinput){
+        maru = maru + "●";
+        passwordinputtextView.setText(maru);
+
+        password = password * 10 + passwordinput;
+
+        inputpasswordmethod(2);
+    }
+
+    public void inputpassword(View v){
+        inputpasswordmethod(1);
+    }
+
+    public void inputpasswordmethod(int judge){
+        sharedPreferences = getSharedPreferences("passwordfirst", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.putInt("passwordfirst",1);
+        editor.commit();
+
+        sharedPreferences = getSharedPreferences("password",Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.putInt("password",password);
+        editor.commit();
+
+        //judge = 1の時はもとの画面に戻る
+        if(judge == 1){
+            intent = new Intent(this,passwordActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
+    public void num1(View v){
+        passwordinput(1);
+    }
+
+    public void num2(View v){
+        passwordinput(2);
+    }
+
+    public void num3(View v){
+        passwordinput(3);
+    }
+
+    public void num4(View v){
+        passwordinput(4);
+    }
+
+    public void num5(View v){
+        passwordinput(5);
+    }
+
+    public void num6(View v){
+        passwordinput(6);
+    }
+
+    public void num7(View v){
+        passwordinput(7);
+    }
+
+    public void num8(View v){
+        passwordinput(8);
+    }
+
+    public void num9(View v){
+        passwordinput(9);
+    }
+
+    public void num0(View v){
+        passwordinput(0);
+    }
+}

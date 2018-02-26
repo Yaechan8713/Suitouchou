c19c9b68676e6586d2d6722bfed6e9afe12d35c6
yaerei <yae.tetsuo@gmail.com>
2018-02-26 21:59:32 +0900

    実行させたら、NoClassDefFoundErrorのエラーが出てしまいました。

--- app/src/main/java/reduce/project/yaerei/suitouchou/MainActivity.java
+++ app/src/main/java/reduce/project/yaerei/suitouchou/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
